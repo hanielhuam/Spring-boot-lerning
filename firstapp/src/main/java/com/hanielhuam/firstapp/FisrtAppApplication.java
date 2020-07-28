571b1ff51dd65a4b3d11e89bd2fc4f64ce7c7225
Haniel Huam <hanielhuam@hotmail.com>
2020-07-28 15:54:11 -0300

    initial commit

---
+++ firstapp/src/main/java/com/hanielhuam/firstapp/FisrtAppApplication.java
@@ -0,0 +1,13 @@
+package com.hanielhuam.firstapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FisrtAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FisrtAppApplication.class, args);
+	}
+
+}
---
+++ firstapp/src/test/java/com/hanielhuam/firstapp/FisrtAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanielhuam.firstapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FisrtAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
